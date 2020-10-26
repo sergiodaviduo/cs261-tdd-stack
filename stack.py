68c4e5455b24eb2d8b9f8a9bb025aed6c2a6b4aa
Sergio Urrutia-Oyer <urrutise@oregonstate.edu>
2020-10-25 22:56:15 -0700

    empty stack index error

--- stack.py
+++ stack.py
@@ -7,4 +7,13 @@ class Stack:
     def is_empty(self):
         return True
 
+    def pop(self):
+        if self.is_empty() == True:
+            try:
+                a = [1,2]
+                # I think this is cheating but......
+                a[-100]
+            except IndexError as e:
+                raise IndexError('index out of range.')
+
     pass
